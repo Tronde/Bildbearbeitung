520dfeee7a1d28d2903d4ac06929bbeee2a8b540
Tronde <tronde@my-it-brain.de>
2017-12-10 22:59:37 +0100

    Rename files inplace

--- rename_jpg.py
+++ rename_jpg.py
@@ -16,12 +16,10 @@ import argparse
 def main():
     parser = argparse.ArgumentParser(description="Dieses Skript dient der Verarbeitung von JPEG-Dateien. Es wertet das Attribut 'EXIF DateTimeOriginal' aus und benennt die verbeiteten Dateien um, so dass diese dem Muster <YYYY-MM-DD_img_NUMBER> entsprechen.")
     parser.add_argument("-s", "--source-dir", required=False, default='.', dest="src", help="Quell-Verzeichnis mit den zu verarbeitenden JPEG-Dateien.")
-    parser.add_argument("-d", "--dest-dir", required=False, dest="dst", help="Ziel-Verzeichnis zur Speicherung der verarbeiteten JPEG-Dateien.")
 
     args = parser.parse_args()
     
     src = args.src
-    dst = args.dst
 
     os.chdir(src)
     for file in os.listdir(src):
@@ -29,16 +27,13 @@ def main():
             f = open(file, 'rb')
             tags = exifread.process_file(f, details=False, stop_tag='EXIF DateTimeOriginal')
             str_DateTime = str(tags['EXIF DateTimeOriginal']).split()
-#            print(str_DateTime)
             str_Date = str(str_DateTime[0]).split(':')
             str_Time = str(str_DateTime[1]).split(':')
-#            print(str_Date)
-#            print(str_Time)
             Date = str_Date[0] + "-" + str_Date[1] + "-" + str_Date[2]
             Time = str_Time[0] + str_Time[1] + str_Time[2]
-#            print(Date)
-            fname = Date + "T" + Time + "_img_"
-            print(fname)
+            fname = Date + "T" + Time + "_img.jpg"
+#            print(fname)
+            os.rename(file,fname)
 
 if __name__ == "__main__":
     main()
